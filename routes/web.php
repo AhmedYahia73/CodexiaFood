c1406122d172cf2d6ceece6bee1d6521ece471e0
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-09-04 21:33:44 +0300

    يبلربي

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', function () {
+    return redirect('/docs/api');
+});
+
+Route::get('/api/docs', function () {
+    return redirect('/docs/api');
+});
